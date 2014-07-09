6906ae35409eb804fc734303b931fc5ef511fcb8
Ahmet Tabanlioglu <ahmet.tabanlioglu@hotmail.com>
2014-07-10 02:33:40 +0300

    socketprogramming-writerclient

---
+++ WriterClient_Computer/WriterClient_Computer.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace WriterClient_Computer
+{
+    partial class WriterClient_Computer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnDurdur = new System.Windows.Forms.Button();
+            this.btnBasla = new System.Windows.Forms.Button();
+            this.txt_Port = new System.Windows.Forms.TextBox();
+            this.performanceCpu = new System.Diagnostics.PerformanceCounter();
+            this.performanceRam = new System.Diagnostics.PerformanceCounter();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtIpAdresi = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.performanceCpu)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.performanceRam)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(149, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Port Numarası";
+            // 
+            // btnDurdur
+            // 
+            this.btnDurdur.Location = new System.Drawing.Point(152, 99);
+            this.btnDurdur.Name = "btnDurdur";
+            this.btnDurdur.Size = new System.Drawing.Size(141, 79);
+            this.btnDurdur.TabIndex = 10;
+            this.btnDurdur.Text = "Durdur";
+            this.btnDurdur.UseVisualStyleBackColor = true;
+            this.btnDurdur.Click += new System.EventHandler(this.btnDurdur_Click);
+            // 
+            // btnBasla
+            // 
+            this.btnBasla.Location = new System.Drawing.Point(17, 99);
+            this.btnBasla.Name = "btnBasla";
+            this.btnBasla.Size = new System.Drawing.Size(129, 79);
+            this.btnBasla.TabIndex = 9;
+            this.btnBasla.Text = "Gönder";
+            this.btnBasla.UseVisualStyleBackColor = true;
+            this.btnBasla.Click += new System.EventHandler(this.btnBasla_Click);
+            // 
+            // txt_Port
+            // 
+            this.txt_Port.Location = new System.Drawing.Point(152, 57);
+            this.txt_Port.Name = "txt_Port";
+            this.txt_Port.Size = new System.Drawing.Size(129, 20);
+            this.txt_Port.TabIndex = 8;
+            // 
+            // performanceCpu
+            // 
+            this.performanceCpu.CategoryName = "Processor Information";
+            this.performanceCpu.CounterName = "% Processor Time";
+            this.performanceCpu.InstanceName = "_Total";
+            // 
+            // performanceRam
+            // 
+            this.performanceRam.CategoryName = "Memory";
+            this.performanceRam.CounterName = "Available MBytes";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.groupBox1.Controls.Add(this.txtIpAdresi);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnDurdur);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txt_Port);
+            this.groupBox1.Controls.Add(this.btnBasla);
+            this.groupBox1.Location = new System.Drawing.Point(12, 14);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(302, 184);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Writer Client";
+            // 
+            // txtIpAdresi
+            // 
+            this.txtIpAdresi.Location = new System.Drawing.Point(15, 57);
+            this.txtIpAdresi.Name = "txtIpAdresi";
+            this.txtIpAdresi.Size = new System.Drawing.Size(131, 20);
+            this.txtIpAdresi.TabIndex = 13;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "İp Adresi";
+            // 
+            // WriterClient_Computer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(328, 210);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "WriterClient_Computer";
+            this.Text = "WriterClient_Computer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.performanceCpu)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.performanceRam)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnDurdur;
+        private System.Windows.Forms.Button btnBasla;
+        private System.Windows.Forms.TextBox txt_Port;
+        private System.Diagnostics.PerformanceCounter performanceCpu;
+        private System.Diagnostics.PerformanceCounter performanceRam;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtIpAdresi;
+        private System.Windows.Forms.Label label2;
+
+    }
+}
+
---
+++ WriterClient_Computer/WriterClient_Computer.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+using System.Net.Sockets;
+using System.Diagnostics;
+using System.Net;
+using System.Runtime.InteropServices;
+
+namespace WriterClient_Computer
+{
+    public partial class WriterClient_Computer : Form
+    {
+        public WriterClient_Computer()
+        {
+            InitializeComponent();
+        }
+        #region gönderilecek bilgileri byte çevirme
+        class property
+        {
+            public string DateTime;
+            public string UserName;
+            public string OperatingSystem;
+            public string cpuUsage;
+            public string Title;
+            public string StackTrace;
+            public string TotalMemory;
+            public string MachineName;
+            public byte[] GetData()
+            {
+                List<byte> result = new List<byte>();
+
+                string[] strings = new string[6] { MachineName, StackTrace, UserName, DateTime, cpuUsage, TotalMemory };
+                foreach (string s in strings)
+                {
+                    
+                    foreach (byte b in Encoding.ASCII.GetBytes(s))
+                        result.Add(b);
+                }
+                return result.ToArray();
+            }
+        #endregion
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+        Socket server1;
+
+        private void btnBasla_Click(object sender, EventArgs e)
+        {
+            btnBasla.Enabled = false;
+            string host = Dns.GetHostName();
+            IPHostEntry ip = Dns.GetHostByName(host);
+            string ipAdresi = ip.AddressList[0].ToString();
+           
+            while (true)
+            {
+                Console.WriteLine("This is a Provider, host name is {0}", Dns.GetHostName());
+
+                //Set the IP address of the server, and its port.
+                //IPEndPoint ipep1 = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);// servere end point noktası açma
+                IPEndPoint ipep1 = new IPEndPoint(IPAddress.Parse(txtIpAdresi.Text), Convert.ToInt32(txt_Port.Text));// servere end point noktası açma
+                try
+                {
+                    while (true)
+                    {
+                        server1 = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                        string cpu = performanceCpu.NextValue().ToString();
+                        string ram = performanceRam.NextValue().ToString();
+
+                        property prop = new property();
+                        prop.MachineName = "Makine Ismi : " + Environment.MachineName + "\n";
+                        prop.StackTrace = "IP Adresi : " + ipAdresi;
+                        prop.DateTime = "\tTarih : " + DateTime.Now.ToString();
+                        prop.UserName = "\tKullanici Adi : " + Environment.UserName;
+                        prop.cpuUsage = "\tCPU : % " + 2;
+                        prop.TotalMemory = "\tKullanilabilir Bellek :" + ram + " MB \n\n\n";
+                        
+
+                        server1.Connect(ipep1);//servere bağlanma
+                        server1.Send(prop.GetData());// bilgileri byte dizisi olarak gönderme
+
+                        Thread.Sleep(1000);//thread uyutma
+
+                    }
+                    server1.Close();// bağlantıyı kapatma
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        private void btnDurdur_Click(object sender, EventArgs e)
+        {
+            server1.Close();
+        }
+    }
+}
